501e636656afeba2c923ba5f0023a154e80e0789
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-27 16:33:57 +0300

    style toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import ButtonPlay from './components/ButtonPlay';
+import Toggle from './components/Toggle';
 
 function App() {
 
   return (
     <div>
       <ButtonPlay />
+
+      <Toggle />
     </div>
   )
 }
