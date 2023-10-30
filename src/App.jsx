3a47af4f24c3c8a506c868907bbdb2e7aca0317a
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-30 16:05:33 +0200

    finish navigation component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css';
 import ButtonPlay from './components/ButtonPlay';
-import Toggle from './components/Toggle';
 import InputWord from './components/InputWord';
+import Navigation from './components/Navigation';
 
 function App() {
 
   return (
     <div className='container'>
+      <Navigation />
       <ButtonPlay />
-
-      <Toggle />
 
       <InputWord />
     </div>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import './Navigation.css';
+import Toggle from '../components/Toggle';
+import logo from '../assets/images/logo.svg';
+import iconArrowDown from '../assets/images/icon-arrow-down.svg'
+
+export default function Navigation() {
+    return (
+        <div className='header flex'>
+            <img src={logo}/>
+            <div className='header-nav flex'>
+                <div className='font-family flex'>
+                    <p className='font-family-style'>Mono</p>
+                    <button className='arrow-down'><img src={iconArrowDown}/></button>
+                </div>
+                <Toggle />
+            </div>     
+        </div>
+    )
+}
