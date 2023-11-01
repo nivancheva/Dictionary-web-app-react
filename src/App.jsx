0a7aca285d23d1611cad79f8fb2c241895bcf05f
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-01 11:13:56 +0200

    add try catch

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ function App() {
   const [getWord, setGetword] = useState();
 
   async function searchWordMeaning(word) {
-      const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-      if (response.ok) {
-        const wordMeaning = await response.json();
-        setGetword(wordMeaning[0]);
-      
-      }  
+      try {
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        if (response.ok) {
+          const wordMeaning = await response.json();
+          setGetword(wordMeaning[0]);    
+        }
+
+      } catch(error) {
+        setGetword(null);
+      }       
   }
 
   function handleSubmit(word) {
